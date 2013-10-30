50bc47557c58fe195318e3e344878d1a22d47929
Sab <sabarish.spk@gmail.com>
2013-10-30 17:02:54 +0530

    Sync

--- thread-messaging-test/src/com/sabscape/threads/messaging/test/Hydrogen.java
+++
@@ -1,9 +0,0 @@
-package com.sabscape.threads.messaging.test;
-
-public class Hydrogen extends Atom {
-
-	public Hydrogen(String id) {
-		super("Hydrogen " + id);
-	}
-
-}
--- thread-messaging-test/src/com/sabscape/threads/messaging/test/TestDisruptor.java
+++
@@ -1,7 +0,0 @@
-package com.sabscape.threads.messaging.test;
-
-public class TestDisruptor {
-	public static void main(String[] args) {
-		
-	}
-}
